ca4b4c137927758eb942e7c18ceaeaa32e81de19
LeoMV123 <leonard.mv03@gmail.com>
2025-01-01 19:14:54 -0600

    [10° COMMIT] Connecting the screens

--- src/virtual/catalog/controller/MainController.java
+++ src/virtual/catalog/controller/MainController.java
@@ -4,12 +4,18 @@
  */
 package virtual.catalog.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -32,11 +38,29 @@ public class MainController implements Initializable {
     }    
 
     @FXML
-    private void ButtonAddCategory(MouseEvent event) {
+    private void ButtonAddCategory(MouseEvent event) throws IOException {
+        ((Node) (event.getSource())).getScene().getRoot().setDisable(true);
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../view/AddCategory.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        
+        stage.setTitle("Add category");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    private void ButtonAddProduct(MouseEvent event) {
+    private void ButtonAddProduct(MouseEvent event) throws IOException {
+        ((Node) (event.getSource())).getScene().getRoot().setDisable(true);
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../view/AddProduct.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        
+        stage.setTitle("Add product");
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
